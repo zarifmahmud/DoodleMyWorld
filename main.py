757a5fe8c5aa8ab6f5d1a4dd1d766fa475b9b5b5
Zarif Mahmud <zarif.mahmud@outlook.com>
2019-08-24 13:35:53 -0400

    Can now recognizes images with Azure, and get Quickdraw images

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ More importantly, add GUI before all this.
 from security import *
 import requests
 from quickdraw import QuickDrawData
+from PIL import Image
+
 import json
 
 
@@ -45,7 +47,12 @@ def bad_sketch(keyword: str):
     Input a noun you want a sketch of, and if Google Quickdraw finds it,
     it will output a drawing
     """
-    pass
+    qd = QuickDrawData()
+
+    key = qd.get_drawing(keyword)
+    filepath = "keyword.gif"
+    key.image.save(filepath)
+    return filepath
 
 
 def bad_sketch_related(keyword: str):
@@ -58,11 +65,29 @@ def bad_sketch_related(keyword: str):
 
 def dream(input_path: str, output_path:str):
     """
-    Puts in an image of
+    The main function, put in an image, and it outputs a sketch
     """
     pass
 
+def speak_your_dream(output_path: str):
+    """
+    Speak to draw, using Azure voice recognition
+    """
+
+def add_to_drawing(word: str):
+    # img = Image.new('RGB', (800, 1280), (255, 255, 255))
+    # img.save("image.png", "PNG")
+    img = Image.open('keyword.gif', 'r')
+    img_w, img_h = img.size
+    background = Image.new('RGBA', (1440, 900), (255, 255, 255, 255))
+    bg_w, bg_h = background.size
+    #offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+    background.paste(img)
+    background.save('image.png', "PNG")
+
 
 if __name__ == '__main__':
-    image_recognizer("bob")
+    # image_recognizer("bob")
     #print(thesaurize("building"))
+    bad_sketch("car")
+    add_to_drawing("tree")
