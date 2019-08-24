4545a39b9b1d5eaed2b3f0dcf46e0ea71a7fc7ce
Zarif Mahmud <zarif.mahmud@outlook.com>
2019-08-24 15:37:50 -0400

    Can paste in images via coordinate now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 TODO: Add thesaurus functionality, speech functionality, grid drawing functionality
 More importantly, add GUI before all this.
+grid system for drawings
 
 """
 from security import *
@@ -37,9 +38,14 @@ def image_recognizer(filepath: str):
                              params=params, json=data)
     response.raise_for_status()
     analysis = response.json()
+    output_dict = {}
+    output_dict["categories"] = analysis["categories"]
+    output_dict["objects"] = analysis["objects"]
+    output_dict["description"] = analysis["description"]
     print(analysis["categories"])
     print(analysis["objects"])
     print(analysis["description"])
+    return output_dict
 
 
 def bad_sketch(keyword: str):
@@ -48,11 +54,13 @@ def bad_sketch(keyword: str):
     it will output a drawing
     """
     qd = QuickDrawData()
-
-    key = qd.get_drawing(keyword)
-    filepath = "keyword.gif"
-    key.image.save(filepath)
-    return filepath
+    try:
+        key = qd.get_drawing(keyword)
+        filepath = "keyword.gif"
+        key.image.save(filepath)
+        return filepath
+    except ValueError:
+        return None
 
 
 def bad_sketch_related(keyword: str):
@@ -62,32 +70,43 @@ def bad_sketch_related(keyword: str):
     If no drawing found, will feed the word through a thesaurus and see if
     Quickdraw
     """
+    pass
+
 
 def dream(input_path: str, output_path:str):
     """
     The main function, put in an image, and it outputs a sketch
     """
-    pass
+    azure_dict = image_recognizer(output_path)
+    object_list = []
+    for obj in azure_dict["objects"]:
+        if bad_sketch(obj["object"]) is not None:
+            img = Image.open('keyword.gif', 'r')
+
+
+
 
 def speak_your_dream(output_path: str):
     """
     Speak to draw, using Azure voice recognition
     """
 
-def add_to_drawing(word: str):
+def add_to_drawing(word: str, xcor, ycor):
     # img = Image.new('RGB', (800, 1280), (255, 255, 255))
     # img.save("image.png", "PNG")
+    bad_sketch(word)
     img = Image.open('keyword.gif', 'r')
     img_w, img_h = img.size
-    background = Image.new('RGBA', (1440, 900), (255, 255, 255, 255))
+    background = Image.new('RGBA', (2000, 2000), (255, 255, 255, 255))
+    # background = Image.open("image.png", "r")
     bg_w, bg_h = background.size
     #offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
-    background.paste(img)
+    background.paste(img, (xcor, ycor))
     background.save('image.png', "PNG")
 
 
 if __name__ == '__main__':
     # image_recognizer("bob")
-    #print(thesaurize("building"))
-    bad_sketch("car")
-    add_to_drawing("tree")
+    # print(thesaurize("building"))
+    # bad_sketch("car")
+    add_to_drawing("star", 0, 500)
